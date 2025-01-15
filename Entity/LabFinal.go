b5b4124df967051ca9bad6f690c207a769229209
JanisataMJ <namuy.2547@gmail.com>
2025-01-15 13:51:07 +0700

    test1

--- Entity/LabFinal.go
+++ Entity/LabFinal.go
@@ -1,5 +1,12 @@
 package entity
 
 import (
-	
-)
+	"gorm.io/gorm"
+) 
+
+type LabFinal struct {
+	gorm.Model
+	SID 	string 	`valid:"required~SID is not required"`
+	Name 	string	`valid:"required~Name is not required"`
+	No 		uint	`valid:"required~No is not required"`
+}
--- Test/LabFinal_test.go
+++ Test/LabFinal_test.go
@@ -1,1 +1,75 @@
 package unit
+
+import (
+	"testing" 
+
+	. "github.com/onsi/gomega"
+	"github.com/asaskevich/govalidator"
+	"github.com/JanisataMJ/FinalLabExam/entity"
+)
+
+func TestValid(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("All Valid", func(t *testing.T) {
+		exam := entity.LabFinal{
+			SID:	"B6515683",
+			Name:	"Janisata",
+			No:		21,
+		}
+
+		ok,err := govalidator.ValidateStruct(exam)
+
+		g.Expect(ok).To(BeTrue())
+		g.Expect(err).To(BeNil())
+	})
+}
+
+func TestNotRequired(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("SID is not required", func(t *testing.T) {
+		exam := entity.LabFinal{
+			SID:	"",
+			Name:	"Janisata",
+			No:		21,
+		}
+
+		ok,err := govalidator.ValidateStruct(exam)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(Equal("SID is not required"))
+	})
+
+	t.Run("Name is not required", func(t *testing.T) {
+		exam := entity.LabFinal{
+			SID:	"65156833",
+			Name:	"",
+			No:		21,
+		}
+
+		ok,err := govalidator.ValidateStruct(exam)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(Equal("Name is not required"))
+	})
+
+	t.Run("No is not required", func(t *testing.T) {
+		exam := entity.LabFinal{
+			SID:	"65156833",
+			Name:	"Janisata",
+			No:		0,
+		}
+
+		ok,err := govalidator.ValidateStruct(exam)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(Equal("No is not required"))
+	})
+}
